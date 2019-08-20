a1b3ffc3f0550e8847686fbd275d8ce75ae639a4
WendyYanto <yanto.wendy98@gmail.com>
2019-08-20 18:07:22 +0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Container
+      v-for="backgroundColor in backgroundColors"
+      :key="backgroundColor"
+      :currentWindowY="currentWindowY"
+      :backgroundColor="backgroundColor"
+    />
   </div>
 </template>
+
+<script>
+import Container from "./Container";
+
+export default {
+  name: "About",
+  components: {
+    Container
+  },
+  data() {
+    return {
+      backgroundColors: ["Red", "Blue", "Green", "Yellow", "Black"],
+      currentWindowY: 0
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateCurrentWindowY);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.updateCurrentWindowY);
+  },
+  methods: {
+    updateCurrentWindowY() {
+      this.currentWindowY = window.scrollY;
+    }
+  }
+};
+</script>
+
+
---
+++ src/views/Container.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container" :style="{'background-color': backgroundColor}">
+    <h2
+      :ref="'currentTitle' + backgroundColor"
+      :style="{'color': 'white','margin': '0', 'background-color': 'black'}"
+    >{{backgroundColor}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Container",
+  props: {
+    backgroundColor: String,
+    currentWindowY: Number
+  },
+  data() {
+    return {
+      isCurrentSticky: false,
+      startTitlePosition: 0,
+      endTitlePosition: 0
+    };
+  },
+  methods: {
+    handleScroll() {
+      const currentRef = this.getCurrentRef(this.backgroundColor);
+      const windowPositionY = this.currentWindowY;
+      if (this.startTitlePosition <= windowPositionY && windowPositionY < this.endTitlePosition) {
+        currentRef.classList.add("sticky");
+      } else {
+        currentRef.classList.remove("sticky");
+      }
+    },
+    getCurrentRef(backgroundColor) {
+      if (backgroundColor === "Red") {
+        return this.$refs.currentTitleRed;
+      } else if (backgroundColor === "Blue") {
+        return this.$refs.currentTitleBlue;
+      } else if (backgroundColor === "Green") {
+        return this.$refs.currentTitleGreen;
+      } else if (backgroundColor === "Yellow") {
+        return this.$refs.currentTitleYellow;
+      } else if (backgroundColor === "Black") {
+        return this.$refs.currentTitleBlack;
+      }
+    }
+  },
+  watch: {
+    currentWindowY() {
+      this.handleScroll();
+    }
+  },
+  mounted() {
+    const currentRef = this.getCurrentRef(this.backgroundColor);
+    this.startTitlePosition = currentRef.offsetTop;
+    this.endTitlePosition = this.startTitlePosition + 400;
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 400px;
+}
+.sticky {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+</style>
